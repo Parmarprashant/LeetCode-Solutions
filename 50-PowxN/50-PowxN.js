416153089c537af454ec589da1fd76d76f5518d8
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:46 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.js
@@ -0,0 +1,10 @@
+// Last updated: 24/04/2026, 10:05:42
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    var ans = Math.pow(x,n);
+    return ans;
+};
