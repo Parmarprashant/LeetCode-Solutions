00c92f9280527169b5f182e02a130d8aca686db4
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:09 +0530

    Create 1894-MergeStringsAlternately

---
+++ 1894-MergeStringsAlternately/1894-MergeStringsAlternately.cpp
@@ -0,0 +1,18 @@
+// Last updated: 24/04/2026, 10:05:05
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string merged = "";
+        
+        for(int i = 0; i < word1.length() || i < word2.length(); i++){
+            if(i < word1.length()){
+                merged += word1[i];
+            }
+            if(i < word2.length()){
+                merged += word2[i];
+            }
+        }
+
+        return merged;
+    }
+};
