843057ad9b0d47516b60bd0c8f12509a49637e2c
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:24 +0530

    Create 342-PowerOfFour

---
+++ 342-PowerOfFour/342-PowerOfFour.js
@@ -0,0 +1,17 @@
+// Last updated: 24/04/2026, 10:05:20
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if (n <= 0) return false;
+
+    while (n % 4 === 0) {
+        n = n / 4;
+    }
+
+    return n === 1;
+};
+
+
+isPowerOfFour(4);
