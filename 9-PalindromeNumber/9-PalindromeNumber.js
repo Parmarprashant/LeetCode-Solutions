e75a3d45c44a20f0446a772009351f88e7f5fccb
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:58 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -0,0 +1,20 @@
+// Last updated: 24/04/2026, 10:05:53
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+
+    let original = x;
+    let rev = 0;
+
+    while (x > 0) {
+        rev = rev * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return rev === original;
+};
+
+isPalindrome(-121);
