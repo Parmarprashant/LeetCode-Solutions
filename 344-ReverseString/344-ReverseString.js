ea219ebf4509bbb075c45a502451cf403948c426
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:29 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/344-ReverseString.js
@@ -0,0 +1,8 @@
+// Last updated: 24/04/2026, 10:05:25
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+  return s.reverse();  
+};
