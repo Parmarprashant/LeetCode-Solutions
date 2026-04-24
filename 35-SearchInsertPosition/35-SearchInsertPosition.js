73d5eef46319f6280f745ffd0a7f6cfea406d2f2
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-04-24 10:05:50 +0530

    Create 35-SearchInsertPosition

---
+++ 35-SearchInsertPosition/35-SearchInsertPosition.js
@@ -0,0 +1,30 @@
+// Last updated: 24/04/2026, 10:05:45
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } 
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        } 
+        else {
+            right = mid - 1;
+        }
+    }
+
+
+    return left;
+};
+
+
+searchInsert([1,3,5,7,9], 9);
