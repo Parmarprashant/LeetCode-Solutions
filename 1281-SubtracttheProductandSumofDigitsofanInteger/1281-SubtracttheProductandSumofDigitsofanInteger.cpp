b0f28d32a18444edfe84ed4198e88fc2cee5177e
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 19:33:02 +0530

    File updated at 03/05/2026, 19:32:58

--- 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
+++ 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
@@ -1,19 +1,19 @@
-// Last updated: 03/05/2026, 19:10:04
+// Last updated: 03/05/2026, 19:32:58
 1class Solution {
 2public:
-3     int subtractProductAndSum(int n) {
-4     int multi = 1;
-5     int sum = 0;
-6     
-7     while(n>0){
-8       int digi = n%10;
-9       n = n/10;
-10       multi = multi*digi;
-11       sum = sum + digi;
-12     }
-13
-14     int result = multi - sum;
+3    int countDigits(int num) {
+4        int count = 0;
+5        int original = num;
+6        while(num>0){
+7            int digi = num%10;
+8            num = num/10;
+9              if(original % digi == 0){
+10                count++;
+11            }
+12        }
+13      
+14
 15
-16      return result;
+16        return count;
 17    }
 18};
