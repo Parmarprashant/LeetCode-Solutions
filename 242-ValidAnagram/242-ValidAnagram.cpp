90b7fa3afbdd76c591077f43fae78624dfb8d7ef
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 23:04:16 +0530

    File updated at 03/05/2026, 23:04:12

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,21 +1,24 @@
-// Last updated: 03/05/2026, 22:37:05
+// Last updated: 03/05/2026, 23:04:12
 1class Solution {
 2public:
-3    bool isPalindrome(int x) {
-4        long long result = 0;
-5        int original = x;
-6        while(x>0){
-7            int digi = x%10;
-8            x = x/10;
-9
-10            result = result * 10 + digi;
-11        }
-12
-13        if(result == original){
-14            return true;
-15        }
-16        else{
-17            return false;
-18        }
-19    }
-20};
+3    string reverseVowels(string s) {
+4     int i =0;
+5     int j = s.size()-1;
+6     while(i<j){
+7      if((s[i] == 'A' || s[i] == 'E'|| s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') && (s[j] == 'A' || s[j] == 'E'|| s[j] == 'I' || s[j] == 'O' || s[j] == 'U' || s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')){
+8        swap(s[i], s[j]);
+9        i++;
+10        j--;
+11      }
+12      else if(!(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')){
+13        i++;
+14      }
+15      else{
+16        j--;
+17      }
+18     }
+19
+20     return s;
+21
+22    }
+23};
