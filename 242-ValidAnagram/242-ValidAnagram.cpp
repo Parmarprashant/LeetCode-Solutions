532324f751b31eaa3de27bd2a5d636f9eac8222a
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 22:37:10 +0530

    File updated at 03/05/2026, 22:37:05

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,21 +1,21 @@
-// Last updated: 03/05/2026, 22:33:18
+// Last updated: 03/05/2026, 22:37:05
 1class Solution {
 2public:
-3    int Square(int n){
-4        int square = 0;
-5        while(n > 0){
-6            int digi = n % 10;
-7            n = n / 10;
-8            square += digi * digi;
-9        }
-10        return square;
-11    }
+3    bool isPalindrome(int x) {
+4        long long result = 0;
+5        int original = x;
+6        while(x>0){
+7            int digi = x%10;
+8            x = x/10;
+9
+10            result = result * 10 + digi;
+11        }
 12
-13    bool isHappy(int n) {
-14     while(n != 1 && n != 4){
-15    n = Square(n);
-16}
-17return n == 1;
-18
+13        if(result == original){
+14            return true;
+15        }
+16        else{
+17            return false;
+18        }
 19    }
 20};
