47e2d790dd10e276d0a4f7290b8f10cba9167bcb
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 20:31:30 +0530

    File updated at 03/05/2026, 20:31:26

--- 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
+++ 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
@@ -1,22 +1,17 @@
-// Last updated: 03/05/2026, 19:51:32
+// Last updated: 03/05/2026, 20:31:26
 1class Solution {
 2public:
-3    int reverseNum(int num) {
-4        int reverse = 0;
+3    int sumBase(int n, int k) {
+4        int result = 0;
 5
-6        while(num > 0){
-7            int digit = num % 10;
-8            reverse = reverse * 10 + digit;
-9            num /= 10;
-10        }
-11
-12        return reverse;
-13    }
-14
-15    bool isSameAfterReversals(int num) {
-16        int first = reverseNum(num);
-17        int second = reverseNum(first);
-18
-19        return second == num;
-20    }
-21};
+6        while(n > 0){
+7            int reminder = n % k;   
+8            int div = n / k;        
+9
+10            result += reminder;     
+11            n = div;                
+12        }
+13
+14        return result;
+15    }
+16};
