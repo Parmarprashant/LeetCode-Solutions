2c9c3940ad990660ffdee6ea75f58d40eeeebbdc
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 22:33:22 +0530

    File updated at 03/05/2026, 22:33:18

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,9 +1,21 @@
-// Last updated: 03/05/2026, 20:37:13
+// Last updated: 03/05/2026, 22:33:18
 1class Solution {
 2public:
-3    bool isAnagram(string s, string t) {
-4        sort(s.begin(), s.end());
-5        sort(t.begin(), t.end());
-6        return (s == t) ? true : false;
-7    }
-8};
+3    int Square(int n){
+4        int square = 0;
+5        while(n > 0){
+6            int digi = n % 10;
+7            n = n / 10;
+8            square += digi * digi;
+9        }
+10        return square;
+11    }
+12
+13    bool isHappy(int n) {
+14     while(n != 1 && n != 4){
+15    n = Square(n);
+16}
+17return n == 1;
+18
+19    }
+20};
