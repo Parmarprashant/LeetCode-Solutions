dfc83971042796bcc6f58c24528016a5f18d971c
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 23:21:53 +0530

    File updated at 03/05/2026, 23:21:48

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,24 +1,22 @@
-// Last updated: 03/05/2026, 23:04:12
+// Last updated: 03/05/2026, 23:21:48
 1class Solution {
 2public:
-3    string reverseVowels(string s) {
-4     int i =0;
-5     int j = s.size()-1;
-6     while(i<j){
-7      if((s[i] == 'A' || s[i] == 'E'|| s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') && (s[j] == 'A' || s[j] == 'E'|| s[j] == 'I' || s[j] == 'O' || s[j] == 'U' || s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')){
-8        swap(s[i], s[j]);
-9        i++;
-10        j--;
-11      }
-12      else if(!(s[i]=='A'||s[i]=='E'||s[i]=='I'||s[i]=='O'||s[i]=='U'||s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')){
-13        i++;
-14      }
-15      else{
-16        j--;
-17      }
-18     }
-19
-20     return s;
-21
-22    }
-23};
+3    bool isPowerOfTwo(int n) {
+4        int i = 2;
+5        long long check = 2;
+6        if(n == 1 || n == 2) return true;
+7       while(n>0){
+8        check = check *i;
+9   
+10        if(check == n){    
+11            return true;
+12        }
+13        else if(check>n){
+14            break;
+15        }
+16
+17        
+18       }
+19        return false;
+20    }
+21};
