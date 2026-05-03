fca920386f3ee336111be9173b3e192230e38c0b
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-03 23:57:29 +0530

    File updated at 03/05/2026, 23:57:25

--- 342-PowerofFour/342-PowerofFour.cpp
+++ 342-PowerofFour/342-PowerofFour.cpp
@@ -1,20 +1,14 @@
-// Last updated: 03/05/2026, 23:45:15
+// Last updated: 03/05/2026, 23:57:25
 1class Solution {
 2public:
-3    bool isPowerOfFour(int n) {
-4        if(n ==4 || n == 1) return true;
-5        int i = 4;
-6        long long check = 4;
-7        while(check<=n){
-8            check = check * i;
-9            if(check == n){
-10                return true;
-11            }
-12            else if(check>n){
-13                break;
-14            }
-15        }
-16
-17        return false;
-18    }
-19};
+3    bool isPerfectSquare(int num) {
+4        long long result;
+5       for(long long i = 1; i<= num; i++){
+6          result = (long long) i* i;
+7          if(result == num){
+8            return true;
+9          }
+10       } 
+11       return false;
+12    }
+13};
