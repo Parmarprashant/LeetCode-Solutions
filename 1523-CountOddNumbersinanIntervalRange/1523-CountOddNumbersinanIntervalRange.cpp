e42f11421bdf29f8aed56f51e121e826948bdac8
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-04 21:25:14 +0530

    File updated at 04/05/2026, 21:25:08

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
@@ -1,22 +1,22 @@
-// Last updated: 04/05/2026, 21:11:45
+// Last updated: 04/05/2026, 21:25:08
 1class Solution {
 2public:
-3    int checkNum(int multi){
-4        if(multi > 0) return 1;
-5        else if(multi < 0) return -1;
-6        else return 0;
-7    }
-8
-9    int arraySign(vector<int>& nums) {
-10        int multi = 1;
-11
-12        for(int i = 0; i < nums.size(); i++){
-13            if(nums[i] == 0) return 0;
-14            multi *= nums[i];
-15            if(multi > 0) multi = 1;
-16            else multi = -1;
+3    int averageValue(vector<int>& nums) {
+4        int count = 0;
+5        int sum = 0;
+6        int avg = 0;
+7
+8        for(int i = 0; i < nums.size(); i++){
+9            if(nums[i] % 2 == 0 && nums[i] % 3 == 0){
+10                sum += nums[i];
+11                count++;
+12            }
+13        }
+14
+15        if(count != 0){
+16            avg = sum / count;
 17        }
 18
-19        return checkNum(multi);
+19        return avg;
 20    }
 21};
