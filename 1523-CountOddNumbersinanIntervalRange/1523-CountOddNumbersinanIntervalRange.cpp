3ad6eb1ba06f541b1b55a3f00924f17bb83ea82b
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-04 21:38:18 +0530

    File updated at 04/05/2026, 21:38:11

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
@@ -1,22 +1,15 @@
-// Last updated: 04/05/2026, 21:25:08
+// Last updated: 04/05/2026, 21:38:11
 1class Solution {
 2public:
-3    int averageValue(vector<int>& nums) {
-4        int count = 0;
-5        int sum = 0;
-6        int avg = 0;
-7
-8        for(int i = 0; i < nums.size(); i++){
-9            if(nums[i] % 2 == 0 && nums[i] % 3 == 0){
-10                sum += nums[i];
-11                count++;
-12            }
-13        }
-14
-15        if(count != 0){
-16            avg = sum / count;
-17        }
-18
-19        return avg;
-20    }
-21};
+3    void reverseString(vector<char>& s) {
+4        int i = 0;
+5        int j = s.size()-1;
+6        while(i<j){
+7            swap(s[i], s[j]);
+8            i++;
+9            j--;
+10            
+11        }
+12
+13            }
+14};
