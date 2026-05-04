e8db172c9c279a581e22b70242acfc87816a9ef6
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-04 20:41:49 +0530

    File updated at 04/05/2026, 20:41:43

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
@@ -1,23 +1,29 @@
-// Last updated: 04/05/2026, 20:26:42
+// Last updated: 04/05/2026, 20:41:43
 1class Solution {
 2public:
-3   int reverseNum(int num){
-4    int reverse = 0;
-5    while(num>0){
-6        int digi = num %10;
-7        num = num/10;
-8        reverse = reverse*10 + digi;
-9    }
-10
-11    return reverse;
-12   }
-13    bool sumOfNumberAndReverse(int num) {
-14        for(int i = 0; i<=num; i++){
-15            if(i + reverseNum(i) == num ){
-16                return true;
-17            }
-18        }
-19
-20        return false;
-21    }
-22};
+3    vector<int> selfDividingNumbers(int left, int right) {
+4        vector<int> result;
+5
+6        for (int i = left; i <= right; i++) {
+7            int temp = i;
+8            bool valid = true;
+9
+10            while (temp > 0) {
+11                int digit = temp % 10;
+12
+13                if (digit == 0 || i % digit != 0) {
+14                    valid = false;
+15                    break;
+16                }
+17
+18                temp /= 10;
+19            }
+20
+21            if (valid) {
+22                result.push_back(i);
+23            }
+24        }
+25
+26        return result;
+27    }
+28};
