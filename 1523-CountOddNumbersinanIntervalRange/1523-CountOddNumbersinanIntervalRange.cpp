2109ca12f86a56184bfddc8f5656e14a1248d8b5
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-04 21:11:51 +0530

    File updated at 04/05/2026, 21:11:45

--- 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
+++ 1523-CountOddNumbersinanIntervalRange/1523-CountOddNumbersinanIntervalRange.cpp
@@ -1,23 +1,22 @@
-// Last updated: 04/05/2026, 20:48:50
+// Last updated: 04/05/2026, 21:11:45
 1class Solution {
 2public:
-3    vector<string> fizzBuzz(int n) {
-4        vector<string> answer;
-5        for(long long i = 1; i<=n; i++){
-6            if(i%3==0 && i%5==0){
-7               answer.push_back("FizzBuzz");
-8            }
-9            else if(i%3==0 && i%5!= 0){
-10                answer.push_back("Fizz");
-11            }
-12            else if(i%5 == 0 && i%3 != 0){
-13                answer.push_back("Buzz");
-14            }
-15            else{
-16                answer.push_back(to_string(i));
-17            }
-18        }
-19
-20        return answer;
-21    }
-22};
+3    int checkNum(int multi){
+4        if(multi > 0) return 1;
+5        else if(multi < 0) return -1;
+6        else return 0;
+7    }
+8
+9    int arraySign(vector<int>& nums) {
+10        int multi = 1;
+11
+12        for(int i = 0; i < nums.size(); i++){
+13            if(nums[i] == 0) return 0;
+14            multi *= nums[i];
+15            if(multi > 0) multi = 1;
+16            else multi = -1;
+17        }
+18
+19        return checkNum(multi);
+20    }
+21};
