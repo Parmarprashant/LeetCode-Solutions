b95c7d34bda2c6c75595212c861de21ed45b26f2
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-05 18:18:56 +0530

    File updated at 05/05/2026, 18:18:49

--- 507-PerfectNumber/507-PerfectNumber.cpp
+++ 507-PerfectNumber/507-PerfectNumber.cpp
@@ -1,22 +1,22 @@
-// Last updated: 05/05/2026, 18:13:33
+// Last updated: 05/05/2026, 18:18:49
 1class Solution {
 2public:
 3    bool isPowerOfTwo(int n) {
-4        int i = 2;
-5        long long check = 2;
-6        if(n == 1 || n == 2) return true;
-7       while(n>0){
-8        check = check *i;
-9   
-10        if(check == n){    
-11            return true;
-12        }
-13        else if(check>n){
-14            break;
-15        }
-16
-17        
-18       }
-19        return false;
+4     int i = 2;
+5     long long check = 2;
+6     if(n==1 || n == 2) return true;
+7     
+8     while(check<=n){
+9        check = check *i;
+10
+11        if(check == n){
+12            return true;
+13        }
+14        else if(check>n){
+15            return false;
+16        }
+17
+18     }
+19     return false;
 20    }
 21};
