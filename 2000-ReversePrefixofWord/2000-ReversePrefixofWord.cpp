0c76e1c3330deaabed0a739965d64c33afa47247
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-05 13:38:35 +0530

    File updated at 05/05/2026, 13:38:29

--- 2000-ReversePrefixofWord/2000-ReversePrefixofWord.cpp
+++ 2000-ReversePrefixofWord/2000-ReversePrefixofWord.cpp
@@ -1,26 +1,18 @@
-// Last updated: 05/05/2026, 12:48:59
+// Last updated: 05/05/2026, 13:38:29
 1class Solution {
 2public:
-3    string reversePrefix(string word, char ch) {
-4        int index = 0;
-5       for(int i = 0; i<word.size(); i++){
-6             if(word[i] == ch){
-7                index = i;
-8                break;
-9             } 
-10       }
-11
-12       if(index == 0){
-13        return word;
-14       }
-15
-16       int left = 0;
-17       int right = index;
-18     while(left<right){
-19        swap(word[left], word[right]);
-20        left++;
-21        right--;
-22     }
-23     return word;
-24    }
-25};
+3    bool isPrefixString(string s, vector<string>& words) {
+4        string temp = "";
+5
+6        for (int i = 0; i < words.size(); i++) {
+7            temp += words[i];
+8
+9            if (temp == s) return true;
+10
+11            
+12            if (temp.length() > s.length()) return false;
+13        }
+14
+15        return false;
+16    }
+17};
