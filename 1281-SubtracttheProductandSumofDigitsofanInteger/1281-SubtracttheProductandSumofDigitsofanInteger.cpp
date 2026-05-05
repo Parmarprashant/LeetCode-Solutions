3ffbc3418367c89bce7caeb662cf2dd8cd21453f
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-05 19:18:36 +0530

    File updated at 05/05/2026, 19:18:29

--- 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
+++ 1281-SubtracttheProductandSumofDigitsofanInteger/1281-SubtracttheProductandSumofDigitsofanInteger.cpp
@@ -1,17 +1,18 @@
-// Last updated: 03/05/2026, 20:31:26
+// Last updated: 05/05/2026, 19:18:29
 1class Solution {
 2public:
-3    int sumBase(int n, int k) {
-4        int result = 0;
-5
-6        while(n > 0){
-7            int reminder = n % k;   
-8            int div = n / k;        
-9
-10            result += reminder;     
-11            n = div;                
-12        }
-13
-14        return result;
-15    }
-16};
+3     int subtractProductAndSum(int n) {
+4      int sum = 0;
+5      int multi = 1;
+6      int result;
+7      while(n>0){
+8        int digi = n%10;
+9        n = n/10;
+10        sum = sum + digi;
+11        multi = multi * digi;
+12        result = multi - sum;
+13      }
+14
+15      return result;
+16    }
+17};
