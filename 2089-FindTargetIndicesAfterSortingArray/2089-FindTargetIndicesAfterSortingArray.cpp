618597f5bb77029c7187c42b5cab32820703d436
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-08 00:30:52 +0530

    File updated at 08/05/2026, 00:30:43

--- 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
+++ 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
@@ -1,24 +1,22 @@
-// Last updated: 08/05/2026, 00:07:36
+// Last updated: 08/05/2026, 00:30:43
 1class Solution {
 2public:
-3    int maximumCount(vector<int>& nums) {
-4        int count1 = 0;
-5        int count2 = 0;
-6        for(long long i = 0; i<nums.size(); i++){
-7            if(nums[i]>0){
-8                count1++;
-9            }
-10            else if(nums[i]<0){
-11                count2++;
-12            }
+3    int specialArray(vector<int>& nums) {
+4      for(int x = 0; x <= nums.size(); x++){
+5
+6    int count = 0;
+7
+8    for(int num : nums){
+9        if(num >= x){
+10            count++;
+11        }
+12    }
 13
-14        }
-15
-16        if(count1>count2){
-17            return count1;
-18        }
-19        
-20
-21        return count2;
-22    }
-23};
+14    if(count == x){
+15        return x;
+16    }
+17}
+18
+19        return -1;
+20    }
+21};
