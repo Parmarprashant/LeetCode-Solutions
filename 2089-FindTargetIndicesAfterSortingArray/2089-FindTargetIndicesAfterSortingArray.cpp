45ec773212d1e3de05302a41cd4305da4bb05ab3
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-08 00:50:44 +0530

    File updated at 08/05/2026, 00:50:34

--- 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
+++ 2089-FindTargetIndicesAfterSortingArray/2089-FindTargetIndicesAfterSortingArray.cpp
@@ -1,24 +1,26 @@
-// Last updated: 08/05/2026, 00:37:26
+// Last updated: 08/05/2026, 00:50:34
 1class Solution {
 2public:
-3    int search(vector<int>& nums, int target) {
+3    int searchInsert(vector<int>& nums, int target) {
 4        int start = 0;
 5        int end = nums.size()-1;
 6        int mid = start + (end - start)/2;
-7        while(start<=end){
-8            if(nums[mid] == target){
-9                return mid;
-10            }
-11            else if(nums[mid]>target){
-12                end = mid-1;
-13            }
-14            else{
-15                start = mid + 1;
-16            }
-17
-18            mid = start + (end - start)/2;
-19        }
-20
-21        return -1;
-22    }
-23};
+7
+8        while(start<=end){
+9            if(nums[mid] == target){
+10                return mid;
+11            }
+12            else if(nums[mid]>target){
+13                end = mid -1;
+14            }
+15            else{
+16                start = mid +1;
+17            }
+18   
+19   mid = start + (end - start)/2;
+20   }
+21       return start;    
+22        }
+23      
+24    
+25};
