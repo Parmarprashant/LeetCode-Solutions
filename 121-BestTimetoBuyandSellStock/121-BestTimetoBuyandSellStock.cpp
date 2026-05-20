86adf6ba2627add2cc272502fd78c90134c01d6a
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-20 17:50:32 +0530

    File updated at 20/05/2026, 17:50:27

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.cpp
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.cpp
@@ -1,16 +1,19 @@
-// Last updated: 19/05/2026, 20:11:38
+// Last updated: 20/05/2026, 17:50:27
 1class Solution {
 2public:
 3    int maxProfit(vector<int>& prices) {
-4        int minimum = prices[0];
-5        int profit = 0;
-6        int cost;
-7        for(int i = 1; i<prices.size(); i++){
-8            cost = prices[i] - minimum;
-9            profit = max(profit, cost);
-10            minimum = min(minimum, prices[i]);
-11        }
+4      int mini = prices[0];
+5      int profit = 0;
+6      int cost;
+7      for(int i = 1; i<prices.size(); i++){
+8        cost = prices[i] - mini;
+9        profit = max(profit, cost);
+10        mini = min(mini, prices[i]);
+11
 12
-13        return profit;
-14    }
-15};
+13
+14      }
+15
+16      return profit;
+17    }
+18};
