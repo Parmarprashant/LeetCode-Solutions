bc9e141e6654a6dd5b5f9b1df68efe1628e834cb
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-05-25 20:59:59 +0530

    File updated at 25/05/2026, 20:59:58

--- 202-HappyNumber/202-HappyNumber.cpp
+++ 202-HappyNumber/202-HappyNumber.cpp
@@ -1,22 +1,21 @@
-// Last updated: 05/05/2026, 18:49:16
+// Last updated: 25/05/2026, 20:59:58
 1class Solution {
 2public:
-3    int Square(int n){
+3    int square(int n){
 4        int sq = 0;
 5        while(n>0){
 6            int digi = n%10;
-7            n = n/10;
-8            sq += digi * digi;
-9        }
-10
-11        return sq;
-12    }
-13    bool isHappy(int n) {
-14     
-15      while(n != 1 && n !=4){
-16         n = Square(n);
-17      } 
-18
-19      return n == 1;
-20    }
-21};
+7            sq +=digi * digi;
+8            n = n/10;
+9            
+10        }
+11
+12        return sq;
+13    }
+14    bool isHappy(int n) {
+15        while(n !=1 && n!=4){
+16            n = square(n);
+17        }
+18        return n == 1;
+19    }
+20};
