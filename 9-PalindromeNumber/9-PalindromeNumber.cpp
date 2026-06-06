8e6b9e0bb26396900be919805ffca1efed972e75
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-06 11:58:47 +0530

    File updated at 06/06/2026, 11:58:45

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -1,21 +1,16 @@
-// Last updated: 05/05/2026, 18:57:23
+// Last updated: 06/06/2026, 11:58:45
 1class Solution {
 2public:
 3    bool isPalindrome(int x) {
-4     long long result = 0;
-5     int original = x;
-6     if(x == 0) return true;
-7     while(x>0){
-8        int digi = x%10; //reminder 
-9        x = x/10; // this modifies the input x
-10        result = result *10 + digi;
+4       int original = x;
+5       long long check = 0;
+6       while(x>0){
+7        int digi = x % 10;
+8        x = x/10;
+9       check = check * 10 + digi;
+10       } 
 11
-12    if(original == result){
-13        return true;
+12
+13       return check == original;
 14    }
-15
-16     }
-17     return false;
-18
-19    }
-20};
+15};
