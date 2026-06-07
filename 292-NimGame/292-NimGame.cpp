554caad79fdab6318e8b8780e7cd8884d21cd446
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-07 17:50:52 +0530

    File updated at 07/06/2026, 17:50:49

--- 292-NimGame/292-NimGame.cpp
+++ 292-NimGame/292-NimGame.cpp
@@ -1,7 +1,9 @@
-// Last updated: 21/05/2026, 23:36:42
+// Last updated: 07/06/2026, 17:50:49
 1class Solution {
 2public:
 3    bool canWinNim(int n) {
-4         return n % 4 != 0;
-5    }
-6};
+4        // if the stones is = 4? it always wins by mh friend!!
+5
+6        return n % 4 !=0;
+7    }
+8};
