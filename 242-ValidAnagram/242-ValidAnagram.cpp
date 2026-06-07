75f9eecc9130121cf78dc9eca65badb52dd86711
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-07 14:36:32 +0530

    File updated at 07/06/2026, 14:36:29

--- 242-ValidAnagram/242-ValidAnagram.cpp
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -1,22 +1,10 @@
-// Last updated: 03/05/2026, 23:21:48
+// Last updated: 07/06/2026, 14:36:29
 1class Solution {
 2public:
-3    bool isPowerOfTwo(int n) {
-4        int i = 2;
-5        long long check = 2;
-6        if(n == 1 || n == 2) return true;
-7       while(n>0){
-8        check = check *i;
-9   
-10        if(check == n){    
-11            return true;
-12        }
-13        else if(check>n){
-14            break;
-15        }
-16
-17        
-18       }
-19        return false;
-20    }
-21};
+3    bool isAnagram(string s, string t) {
+4      sort(t.begin(), t.end());
+5      sort(s.begin(), s.end());
+6
+7      return s == t;
+8    }
+9};
