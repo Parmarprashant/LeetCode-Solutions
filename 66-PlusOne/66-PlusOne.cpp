a37bca92744afb116daff973c128707cd70525d4
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-07 15:01:31 +0530

    File updated at 07/06/2026, 15:01:28

--- 66-PlusOne/66-PlusOne.cpp
+++ 66-PlusOne/66-PlusOne.cpp
@@ -1,17 +1,18 @@
-// Last updated: 01/06/2026, 21:52:14
+// Last updated: 07/06/2026, 15:01:28
 1class Solution {
 2public:
 3    vector<int> plusOne(vector<int>& digits) {
-4        for (int i = digits.size() - 1; i >= 0; i--) {
-5            if (digits[i] < 9) {
-6                digits[i]++;
-7                return digits;
-8            }
+4      for(int i = digits.size()-1; i>=0; i--){
+5        if(digits[i]<9){
+6            digits[i]++;
+7            return digits;
+8        }
 9
-10            digits[i] = 0;
-11        }
+10        digits[i] = 0;
+11      }
 12
-13        digits.insert(digits.begin(), 1);
-14        return digits;
-15    }
-16};
+13      digits.insert(digits.begin(), 1);
+14
+15      return digits;
+16    }
+17};
