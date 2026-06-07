18b7de318b671e076df53547b8e7edba337c6d72
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-07 17:31:28 +0530

    File updated at 07/06/2026, 17:31:25

--- 263-UglyNumber/263-UglyNumber.cpp
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -1,13 +1,13 @@
-// Last updated: 26/05/2026, 23:54:06
+// Last updated: 07/06/2026, 17:31:25
 1class Solution {
 2public:
 3    bool isUgly(int n) {
-4        if (n <= 0) return false;
+4    if(n<= 0) return false;
 5
-6        while (n % 2 == 0) n /= 2;
-7        while (n % 3 == 0) n /= 3;
-8        while (n % 5 == 0) n /= 5;
+6    while(n%2 == 0) n/=2;
+7    while(n%3 == 0) n /= 3;
+8    while(n % 5 == 0) n/=5;
 9
-10        return n == 1;
+10    return n == 1;
 11    }
 12};
