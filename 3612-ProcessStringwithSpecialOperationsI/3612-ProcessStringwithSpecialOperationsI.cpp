537d21b8faf1e126ba4902bad1b8d5a257e72a61
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-17 20:31:37 +0530

    File updated at 17/06/2026, 20:31:35

--- 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
+++ 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
@@ -1,20 +1,41 @@
-// Last updated: 17/06/2026, 11:14:24
-1class Solution {
-2public:
-3    int balancedStringSplit(string s) {
-4        int balance = 0;
-5        int count = 0;
-6
-7        for(char ch : s) {
-8            if(ch == 'L')
-9                balance++;
-10            else
-11                balance--;
-12
-13            if(balance == 0)
-14                count++;
-15        }
-16
-17        return count;
-18    }
-19};
+// Last updated: 17/06/2026, 20:31:35
+#define RES_LEN     4048 * 1024
+
+char* processStr(char* s) {
+    char *result = calloc(RES_LEN, sizeof(char));   
+    int rLen = 0, l, r;
+
+    for(int i = 0; s[i]; i++) {
+        switch(s[i]) {
+            case '*': // remove last character from result
+                    if(rLen > 0) {
+                        --rLen;
+                        result[rLen] = '\0';
+                    }
+                    break;
+
+            case '#': // duplicate the current result and append it to itself
+                    rLen += sprintf(result + rLen, "%s", result);
+                    result[rLen] = '\0';  
+                    break;
+
+            case '%': // reverse the current result
+                    l = 0, r = rLen-1;
+                    while(l < r) {
+                        int temp = result[l];
+                        result[l] = result[r];
+                        result[r] = temp;
+                        ++l, --r;
+                      }
+                      result[rLen] = '\0';
+                    break;
+            
+            default: // letter is lowercase, append it to result 
+                    result[rLen++] = s[i];
+                    result[rLen] = '\0';            
+                    break;
+        }   
+    }
+    result[rLen] = '\0';
+    return result;
+}
