7edc5ca4bdb8a7370de6ed44eed6c51905d2bc7d
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-17 21:32:07 +0530

    File updated at 17/06/2026, 21:32:05

--- 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
+++ 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
@@ -1,24 +1,13 @@
-// Last updated: 17/06/2026, 21:12:28
+// Last updated: 17/06/2026, 21:32:05
 1class Solution {
 2public:
-3    string greatestLetter(string s) {
-4        string ans = "";
-5
-6        for(char c = 'Z'; c>= 'A'; c--){
-7            bool upper = false;
-8            bool lower = false;
-9
-10            for(char ch: s){
-11                if(ch == c) upper = true;
-12                if(ch == c + 32) lower = true;
-13            }
-14
-15            if(upper && lower){
-16                ans += c;
-17                return ans;
-18            }
-19        }
-20
-21        return "";
-22    }
-23};
+3    int maximumProduct(vector<int>& nums) {
+4    sort(nums.begin(), nums.end());
+5    int n = nums.size();
+6
+7    return max(
+8        nums[n-1] * nums[n-2] * nums[n-3],
+9        nums[0] * nums[1]* nums[n-1]
+10    );
+11    }
+12};
