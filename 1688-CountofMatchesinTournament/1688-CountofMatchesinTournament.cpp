6416694a269fe2b3eae4e8b7e792a69d513516bc
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-17 20:17:04 +0530

    File updated at 17/06/2026, 20:17:02

--- 1688-CountofMatchesinTournament/1688-CountofMatchesinTournament.cpp
+++ 1688-CountofMatchesinTournament/1688-CountofMatchesinTournament.cpp
@@ -1,26 +1,7 @@
-// Last updated: 17/06/2026, 11:47:16
+// Last updated: 17/06/2026, 20:17:02
 1class Solution {
 2public:
 3    int numberOfMatches(int n) {
-4      int leftteams ;
-5      int matches;
-6      int sum = 0;
-7      while(n>1){
-8        if(n%2 != 0){
-9           matches = (n-1)/2;
-10           leftteams = n-matches;
-11
-12           sum += matches;
-13           n = leftteams;
-14        }
-15        else{
-16
-17            matches = n/2;
-18            leftteams = n - matches;
-19            sum += matches;
-20            n = leftteams;
-21        }
-22      }  
-23      return sum;
-24    }
-25};
+4    return n-1;
+5    }
+6};
