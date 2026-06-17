ee086dc17bc8dd320d3ee45bc922951a74a41726
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-17 11:14:26 +0530

    File updated at 17/06/2026, 11:14:24

--- 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
+++ 3612-ProcessStringwithSpecialOperationsI/3612-ProcessStringwithSpecialOperationsI.cpp
@@ -1,30 +1,20 @@
-// Last updated: 17/06/2026, 10:57:41
+// Last updated: 17/06/2026, 11:14:24
 1class Solution {
 2public:
-3    string processStr(string s) {
-4        string result;
-5
+3    int balancedStringSplit(string s) {
+4        int balance = 0;
+5        int count = 0;
 6
-7        for(int i = 0; i<s.length(); i++){
-8           if(s[i] == '*'){
-9            if(!result.empty()){
-10            result.pop_back();
-11            }
-12           }
-13           else if(s[i] == '#'){
-14            result += result;
-15           }
-16           else if(s[i] == '%'){
-17            reverse(result.begin(), result.end());
-18           }
-19           else{
-20
-21            result += s[i];
-22           }
-23        }
-24
-25
-26        return result;
-27        
-28    }
-29};
+7        for(char ch : s) {
+8            if(ch == 'L')
+9                balance++;
+10            else
+11                balance--;
+12
+13            if(balance == 0)
+14                count++;
+15        }
+16
+17        return count;
+18    }
+19};
