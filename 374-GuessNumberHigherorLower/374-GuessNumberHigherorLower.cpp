fc4e05c81f66662c29bd6fd3c4234f0811bb9400
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-17 21:12:30 +0530

    File updated at 17/06/2026, 21:12:28

--- 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
+++ 374-GuessNumberHigherorLower/374-GuessNumberHigherorLower.cpp
@@ -1,13 +1,24 @@
-// Last updated: 17/06/2026, 21:07:04
+// Last updated: 17/06/2026, 21:12:28
 1class Solution {
 2public:
-3    string toLowerCase(string s) {
-4       for(int i = 0; i<s.size(); i++){
-5        if(s[i]>='A' && s[i]<='Z'){
-6            s[i] = s[i] + 32;
-7        }
-8       
-9       } 
-10        return s;
-11    }
-12};
+3    string greatestLetter(string s) {
+4        string ans = "";
+5
+6        for(char c = 'Z'; c>= 'A'; c--){
+7            bool upper = false;
+8            bool lower = false;
+9
+10            for(char ch: s){
+11                if(ch == c) upper = true;
+12                if(ch == c + 32) lower = true;
+13            }
+14
+15            if(upper && lower){
+16                ans += c;
+17                return ans;
+18            }
+19        }
+20
+21        return "";
+22    }
+23};
