b84ae6d27174e3358a2d0440e47263176c024d3e
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-23 13:27:31 +0530

    File updated at 23/06/2026, 13:27:30

--- 88-MergeSortedArray/88-MergeSortedArray.cpp
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -1,17 +1,29 @@
-// Last updated: 23/06/2026, 13:26:36
-1class Solution {
-2public:
-3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-4        int i = m - 1, j = n - 1, k = m + n - 1;
-5
-6        while (i >= 0 && j >= 0) {
-7            if (nums1[i] > nums2[j])
-8                nums1[k--] = nums1[i--];
-9            else
-10                nums1[k--] = nums2[j--];
-11        }
-12
-13        while (j >= 0)
-14            nums1[k--] = nums2[j--];
-15    }
-16};
+// Last updated: 23/06/2026, 13:27:30
+1/**
+2 * Definition for a binary tree node.
+3 * struct TreeNode {
+4 *     int val;
+5 *     TreeNode *left;
+6 *     TreeNode *right;
+7 *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+8 *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+9 *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14     vector<int> ans;
+15
+16    void dfs(TreeNode* root) {
+17        if (!root) return;
+18
+19        dfs(root->left);
+20        ans.push_back(root->val);
+21        dfs(root->right);
+22    }
+23
+24    vector<int> inorderTraversal(TreeNode* root) {
+25        dfs(root);
+26        return ans;
+27    }
+28};
