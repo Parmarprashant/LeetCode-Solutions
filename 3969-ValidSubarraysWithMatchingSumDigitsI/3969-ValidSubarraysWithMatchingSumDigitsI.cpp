de65ab804fbb2a7e9d3404fe4c96b5f902d55dca
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-06-24 10:06:53 +0530

    File updated at 24/06/2026, 10:06:50

--- 3969-ValidSubarraysWithMatchingSumDigitsI/3969-ValidSubarraysWithMatchingSumDigitsI.cpp
+++ 3969-ValidSubarraysWithMatchingSumDigitsI/3969-ValidSubarraysWithMatchingSumDigitsI.cpp
@@ -1,31 +1,18 @@
-// Last updated: 24/06/2026, 10:05:53
+// Last updated: 24/06/2026, 10:06:50
 1class Solution {
 2public:
-3    bool valid(long long sum, int x) {
-4        if (sum % 10 != x) return false;
+3    int maxNumberOfBalloons(string text) {
+4        vector<int> cnt(26, 0);
 5
-6        long long first = sum;
-7        while (first >= 10)
-8            first /= 10;
-9
-10        return first == x;
-11    }
-12
-13    int countValidSubarrays(vector<int>& nums, int x) {
-14        int n = nums.size();
-15        int ans = 0;
-16
-17        for (int i = 0; i < n; i++) {
-18            long long sum = 0;
-19
-20            for (int j = i; j < n; j++) {
-21                sum += nums[j];
-22
-23                if (valid(sum, x))
-24                    ans++;
-25            }
-26        }
-27
-28        return ans;
-29    }
-30};
+6        for(char c : text)
+7            cnt[c - 'a']++;
+8
+9        return min({
+10            cnt['b' - 'a'],
+11            cnt['a' - 'a'],
+12            cnt['l' - 'a'] / 2,
+13            cnt['o' - 'a'] / 2,
+14            cnt['n' - 'a']
+15        });
+16    }
+17};
