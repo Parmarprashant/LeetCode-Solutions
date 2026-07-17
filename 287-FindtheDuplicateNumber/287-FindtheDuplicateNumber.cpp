b1f16e705fdb944d07e1440b17b943eb4371c4e7
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-07-17 14:46:15 +0530

    File updated at 17/07/2026, 14:46:15

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.cpp
@@ -1,15 +1,17 @@
-// Last updated: 17/07/2026, 14:39:02
+// Last updated: 17/07/2026, 14:46:15
 1class Solution {
 2public:
 3    int findDuplicate(vector<int>& nums) {
 4        sort(nums.begin(), nums.end());
-5
-6        for (int i = 1; i < nums.size(); i++) {
-7            if (nums[i] == nums[i-1]) {
-8                return nums[i];
-9            }
-10        }
-11
-12        return -1;
-13    }
-14};
+5        int ans;
+6        for (int i = 0; i < nums.size(); i++) {
+7           ans = nums[i] ^ nums[i+1];
+8
+9           if(ans == 0){
+10            return nums[i];
+11           }
+12        }
+13
+14        return -1;
+15    }
+16};
