dedc8810fd7944b6cc7568b8f46beea6c2de46d4
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-07-23 13:51:10 +0530

    File updated at 23/07/2026, 13:51:06

--- 896-MonotonicArray/896-MonotonicArray.cpp
+++ 896-MonotonicArray/896-MonotonicArray.cpp
@@ -1,18 +1,19 @@
-// Last updated: 23/07/2026, 13:44:04
+// Last updated: 23/07/2026, 13:51:06
 1class Solution {
 2public:
 3    bool isMonotonic(vector<int>& nums) {
-4        bool increasing = true;
-5        bool decreasing = true;
+4        bool inc = true;
+5        bool dec = true;
 6
-7        for (int i = 1; i < nums.size(); i++) {
-8            if (nums[i] < nums[i - 1])
-9                increasing = false;
+7        for(int i = 1; i<nums.size(); i++){
+8             if (nums[i] < nums[i - 1])
+9                inc = false;
 10
 11            if (nums[i] > nums[i - 1])
-12                decreasing = false;
+12                dec = false;
 13        }
 14
-15        return increasing || decreasing;
-16    }
-17};
+15
+16        return dec || inc;
+17    }
+18};
