a8bdbaa51a33fdcf007b840d59c191b8cac2f41a
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-07-23 13:33:24 +0530

    File updated at 23/07/2026, 13:33:20

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.cpp
@@ -1,24 +1,22 @@
-// Last updated: 23/07/2026, 13:24:05
+// Last updated: 23/07/2026, 13:33:20
 1class Solution {
 2public:
 3    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-4      vector<int> sorted = candies;
-5      sort(sorted.begin(), sorted.end());
-6      int main = sorted.back();
-7      vector<bool> s;
-8      int check;
-9      
-10      for(int i = 0; i<candies.size(); i++){
-11         check = extraCandies + candies[i];
-12
-13         if(check>=main){
-14            s.push_back(true);
-15         }
-16         else{
-17            s.push_back(false);
-18         }
-19      }
-20
-21      return s;
-22    }
-23};
+4     int max_ele = *max_element(candies.begin(), candies.end());
+5      vector<bool> s;
+6      int check;
+7      
+8      for(int i = 0; i<candies.size(); i++){
+9         check = extraCandies + candies[i];
+10
+11         if(check>=max_ele){
+12            s.push_back(true);
+13         }
+14         else{
+15            s.push_back(false);
+16         }
+17      }
+18
+19      return s;
+20    }
+21};
