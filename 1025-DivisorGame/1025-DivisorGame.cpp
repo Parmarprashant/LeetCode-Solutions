9d77e62d5056c777a2c46bdcb013ee0d566224eb
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-07-28 13:02:33 +0530

    File updated at 28/07/2026, 13:02:32

--- 1025-DivisorGame/1025-DivisorGame.cpp
+++ 1025-DivisorGame/1025-DivisorGame.cpp
@@ -1,7 +1,11 @@
-// Last updated: 02/06/2026, 14:14:48
+// Last updated: 28/07/2026, 13:02:32
 1class Solution {
 2public:
 3    bool divisorGame(int n) {
-4        return n % 2 == 0;
-5    }
-6};
+4        if(n%2 ==0){
+5            return true;
+6        }
+7
+8        return false;
+9    }
+10};
