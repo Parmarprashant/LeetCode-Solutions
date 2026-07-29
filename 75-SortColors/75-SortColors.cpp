19cbe70ad9ed62b5a0d29e4f62aa95a7cb4513d2
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-07-29 14:17:12 +0530

    File updated at 29/07/2026, 14:17:12

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,7 +1,15 @@
-// Last updated: 19/05/2026, 19:52:03
+// Last updated: 29/07/2026, 14:17:12
 1class Solution {
 2public:
 3    void sortColors(vector<int>& nums) {
-4        return sort(nums.begin(), nums.end());
-5    }
-6};
+4          int n = nums.size();
+5
+6        for(int i = 0; i < n - 1; i++) {
+7            for(int j = 0; j < n - i - 1; j++) {
+8                if(nums[j] > nums[j + 1]) {
+9                    swap(nums[j], nums[j + 1]);
+10                }
+11            }
+12        }   
+13    }
+14};
