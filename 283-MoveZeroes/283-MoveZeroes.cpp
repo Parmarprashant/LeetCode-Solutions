a50c7a4b979ecd415d29cdd5acc7ece4888f0b7d
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-08-07 12:54:24 +0530

    File updated at 07/08/2026, 12:54:24

--- 283-MoveZeroes/283-MoveZeroes.cpp
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -1,13 +1,31 @@
-// Last updated: 07/06/2026, 17:44:35
+// Last updated: 07/08/2026, 12:54:24
 1class Solution {
 2public:
 3    void moveZeroes(vector<int>& nums) {
-4        int j = 0;
-5        for(int i = 0; i<nums.size(); i++){
-6            if(nums[i] != 0){
-7                swap(nums[i], nums[j]);
-8                j++;
-9            }
-10        }
-11    }
-12};
+4        vector<int> temp;
+5   
+6        int n = nums.size();
+7          for(int i : nums){
+8            if(i != 0){
+9                temp.push_back(i);
+10                
+11         }
+12          }
+13
+14
+15         int nonZ = temp.size();
+16
+17         for(int i = 0; i<temp.size(); i++){
+18            nums[i] = temp[i];
+19         }
+20         
+21
+22         for(int i = nonZ; i<n; i++){
+23            nums[i] = 0;
+24         }
+25
+26
+27    
+28
+29      }
+30};
