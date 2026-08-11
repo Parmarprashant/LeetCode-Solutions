52259201b6f49aeb526983f59eba1d1e5b9c0949
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-08-11 22:23:09 +0530

    File updated at 11/08/2026, 22:23:07

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,28 +1,20 @@
-// Last updated: 11/08/2026, 17:02:45
+// Last updated: 11/08/2026, 22:23:07
 1class Solution {
 2public:
-3    void sortColors(vector<int>& nums) {
-4    int cnt0 = 0;
-5    int cnt1 = 0;
-6    int cnt2 = 0;
-7    int n = nums.size();
-8
-9    for(int i = 0; i<n; i++){
-10        if(nums[i] == 0) cnt0++;
-11        else if(nums[i] == 1) cnt1++;
-12        else cnt2++;
-13    }
-14
-15    for(int i = 0; i<cnt0; i++){
-16        nums[i] = 0;
-17    }
-18    for(int i = cnt0; i< cnt0 + cnt1; i++){
-19        nums[i] = 1;
-20    }
-21    for(int i = cnt0 + cnt1; i<n; i++){
-22        nums[i] = 2;
-23    }
-24
-25  
-26    }
-27};
+3    int majorityElement(vector<int>& nums) {
+4       int n = nums.size();
+5
+6       for(int i = 0; i<nums.size(); i++){
+7          int count = 0;
+8          for(int j = 0; j<nums.size(); j++){
+9            if(nums[i] == nums[j]){
+10                count++;
+11            }
+12          }
+13          if(count>n/2){
+14            return nums[i];
+15          }
+16       }
+17       return -1;
+18    }
+19};
