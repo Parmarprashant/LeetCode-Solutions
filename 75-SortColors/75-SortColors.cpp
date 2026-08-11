49f15e5438f24457a58627b2dede089ad95a1a5f
Prashant Parmar <prashant.parmar.cg@gmail.com>
2026-08-11 22:38:34 +0530

    File updated at 11/08/2026, 22:38:32

--- 75-SortColors/75-SortColors.cpp
+++ 75-SortColors/75-SortColors.cpp
@@ -1,20 +1,19 @@
-// Last updated: 11/08/2026, 22:23:07
+// Last updated: 11/08/2026, 22:38:32
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
-4       int n = nums.size();
-5
-6       for(int i = 0; i<nums.size(); i++){
-7          int count = 0;
-8          for(int j = 0; j<nums.size(); j++){
-9            if(nums[i] == nums[j]){
-10                count++;
-11            }
-12          }
-13          if(count>n/2){
-14            return nums[i];
-15          }
-16       }
-17       return -1;
-18    }
-19};
+4      map<int, int> mp;
+5      int n = nums.size();
+6      for(int i = 0; i<n; i++){
+7        mp[nums[i]]++;
+8      }
+9
+10      for(auto it: mp){
+11        if(it.second>n/2){
+12            return it.first;
+13        }
+14      }
+15
+16      return -1;
+17    }
+18};
